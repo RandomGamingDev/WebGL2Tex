910919810a4dc42512ac8cd798f6b369bbe39450
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-06-09 11:56:23 -0400

    the actual code stuff

---
+++ tex.js
@@ -0,0 +1,166 @@
+let Texture = {};
+
+Texture.Base = class {
+  constructor(tex, bufType, gl) {
+    this.tex = tex;
+    this.bufType = bufType;
+    this.gl = gl;
+  }
+  
+  setUniform(shad, uniName) {
+    this.activate();
+    this.gl.uniform1i(this.gl.getUniformLocation(shad, uniName), this.slot);
+  }
+  
+  paramI(param, val) {
+    this.gl.texParameteri(this.bufType, param, val);
+  }
+  
+  paramF(param, val) {
+    this.gl.texParameterf(this.bufType, param, val);
+  }
+  
+  genMipmap() {
+    this.gl.generateMipmap(this.gl.bufType);
+  }
+  
+  bind() {
+    this.gl.bindTexture(this.bufType, this.tex);
+  }
+  
+  unbind() {
+    this.gl.bindTexture(this.bufType, null);
+  }
+  
+  activate() {
+    this.gl.activeTexture(this.gl.TEXTURE0 + this.slot);
+  }
+  
+  delete() {
+    this.gl.deleteTexture(this.tex);
+  }
+}
+
+Texture.T2D = class extends Texture.Base {
+  constructor(level,
+              internalFormat,
+              w, h,
+              slot,
+              border, srcFormat,
+              srcType, pixel,
+              gl) {
+    super(gl.createTexture(), gl.TEXTURE_2D, gl);
+    
+    this.bind();
+    this.slot = slot;
+    this.activate();
+    this.data(
+      level,
+      internalFormat,
+      w,
+      h,
+      border,
+      srcFormat,
+      srcType,
+      pixel
+    );
+  }
+  
+  data(level,
+       internalFormat,
+       w, h,
+       border, srcFormat,
+       srcType, pixel) {
+    this.gl.texImage2D(
+      this.bufType,
+      level,
+      internalFormat,
+      w,
+      h,
+      border,
+      srcFormat,
+      srcType,
+      pixel
+    );
+  }
+  
+  subData(level,
+          xoffset,
+          yoffset,
+          w, h,
+          format,
+          type,
+          source) {
+    this.gl.texSubImage2D(this.bufType,
+                          level,
+                          xoffset,
+                          yoffset,
+                          w,
+                          h,
+                          format,
+                          type,
+                          source);
+  }
+}
+
+Texture.T3D = class extends Texture.Base {
+    constructor(level,
+                internalFormat,
+                w, h, l,
+                slot,
+                border, srcFormat,
+                srcType, pixel,
+                gl) {
+      super(gl.createTexture(), gl.TEXTURE_3D, gl);
+      this.bind();
+      this.slot = slot;
+      this.activate();
+      this.data(
+        level,
+        internalFormat,
+        w, h, l,
+        border,
+        srcFormat,
+        srcType,
+        pixel
+      );
+    }
+    
+    data(level,
+         internalFormat,
+         w, h, l,
+         border, srcFormat,
+         srcType, pixel) {
+      this.gl.texImage3D(
+        this.bufType,
+        level,
+        internalFormat,
+        w, h, l,
+        border,
+        srcFormat,
+        srcType,
+        pixel
+      );
+    }
+    
+    subData(level,
+            xoffset,
+            yoffset,
+            zoffset,
+            w, h, l,
+            format,
+            type,
+            source) {
+      this.gl.texSubImage3D(this.bufType,
+                            level,
+                            xoffset,
+                            yoffset,
+                            zoffset,
+                            w,
+                            h,
+                            l,
+                            format,
+                            type,
+                            source);
+    }
+}
